4441adfffd68f28d1bf210f685323c7660a5229e
Teoalmonte <teoalmonte@gmail.com>
2018-06-20 18:25:24 -0400

    protected routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import jwt_decode from 'jwt-decode'
 import setAuthToken from './utils/setAuthToken'
-import { setCurrentUser } from './actions/authActions'
+import { setCurrentUser, logoutUser } from './actions/authActions'
 
 import { Provider } from 'react-redux';
 import store from './store';
@@ -19,6 +19,11 @@ if(localStorage.jwtToken) {
   setAuthToken(localStorage.jwtToken)
   const decoded = jwt_decode(localStorage.jwtToken)
   store.dispatch(setCurrentUser(decoded))
+  const currentTime = Date.now() / 100000000
+  if (decoded.exp < currentTime) {
+    store.dispatch(logoutUser())
+    window.location.href = '/login'
+  }
 }
 
 class App extends Component {
